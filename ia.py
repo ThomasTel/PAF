63408063c5aaa490b6a94f130f8391b2f01b2d5a
Thomas Tellier <thomastel94@hotmail.fr>
2015-06-23 02:25:20 +0200

    IA qui marche

--- ia.py
+++ ia.py
@@ -1,5 +1,4 @@
 #à orienter objet
-#algo idiot (pas pensé au bord), se contenter d'un simple parcours en largeur
 from genere_tests import *
 
 def isIn(lin, col, nbLin, nbCol):
@@ -14,38 +13,51 @@ def inRect(lin, col, a, b, nbLinsCell, nbColsCell):
 def but(a, b, nbLinsCell, nbColsCell, t):
    nbLin = len(t)
    nbCol = len(t[0])
-   r = [[9] * nbCol for _ in range(nbLin)]
-   for k in range((min(nbLinsCell, nbColsCell)+1)//2):
-      for _ in range(2):
-         for i in range(a+k, a+nbLinsCell-k):
-            for j in [b+k, b+nbColsCell-k]:
-               m = 0
-               n = 0
-               for x, y in vois(i, j) :
-                  if not isIn(x, y, nbLin, nbCol):
-                     continue
-                  if k == 0:
-                     if not inRect(x, y, a, b, nbLinsCell, nbColsCell):
-                        m += t[x][y]
-                        n += 1
-                  elif r[x-(a+k)][y-(b+k)] != -1:
-                     m += r[x-(a+k)][y-(b+k)]
+   r = [[0] * nbCol for _ in range(nbLin)]
+   vu = [[False] * nbCol for _ in range(nbLin)]
+   q = []
+   for i in range(nbLinsCell):
+      if b != 0:
+         q.append((a+i, b))
+      if b+nbColsCell != nbCol:
+         q.append((a+i, b+nbColsCell-1))
+   for j in range(nbColsCell):
+      if a != 0:
+         q.append((a, b+j))
+      if a+nbLinsCell != nbLin:
+         q.append((a+nbLinsCell-1, b+j))
+   while q:
+      nq = []
+      for i, j in q:
+         if not vu[i-a][j-b]:
+            n = 0
+            s = 0
+            for x, y in vois(i, j):
+               if isIn(x, y, nbLin, nbCol):
+                  if not inRect(x, y, a, b, nbLinsCell, nbColsCell):
+                     s += t[x][y]
                      n += 1
-               if n > 0:
-                  m = int(m / n + 0.5)
-                  r[i-(a+k)][j-(b+k)] = m
-         a, b = b, a
-         nbLinsCell, nbColsCell = nbColsCell, nbLinsCell
+                  elif vu[x-a][y-b]:
+                     s += r[x-a][y-b]
+                     n += 1
+                  else:
+                     nq.append((x, y))
+            if n != 0: #impossible
+               r[i-a][j-b] = int(s / n + 0.5) - (0 if n % 2 == 1 else rand(0))
+      for i, j in q:
+         vu[i-a][j-b] = True
+      q = nq[:]
    return r
-
+   
 nbCellsLin, nbCellsCol, nbColsCell, nbLinsCell = genereDims()
 t = genereMat(nbCellsLin, nbCellsCol, nbColsCell, nbLinsCell)
 afficheMat(nbColsCell, nbLinsCell, t)
-a = rand(nbCellsLin) * nbCellsLin
-b = rand(nbCellsCol) * nbCellsCol
-r = but(a, b, nbLinsCell, nbColsCell, t)
+a = rand(nbCellsLin) * nbLinsCell
+b = rand(nbCellsCol) * nbColsCell
 print()
-print(a, b)
+print(a//nbLinsCell, b//nbColsCell)
+print()
+r = but(a, b, nbLinsCell, nbColsCell, t)
 for i in range(nbLinsCell):
    for j in range(nbColsCell):
       print(r[i][j], end="")
